f3590f18dd6621d2870bfeef58832dc0068695aa
Andrey Laptev <andrii.laptiev@devoxsoftware.com>
2021-03-13 13:39:33 +0200

    repository pattern implementation

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonConvert.DeserializeObject<List<ProductBrand>>(brandsData);
+
+                    foreach (var item in brands)
+                    {
+                        await context.ProductBrands.AddAsync(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.ProductTypes.Any())
+                {
+                    var typesData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/types.json");
+                    var types = JsonConvert.DeserializeObject<List<ProductType>>(typesData);
+
+                    foreach (var item in types)
+                    {
+                        await context.ProductTypes.AddAsync(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonConvert.DeserializeObject<List<Product>>(productsData);
+
+                    foreach (var item in products)
+                    {
+                        await context.Products.AddAsync(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(e.Message);
+            }
+        }
+    }
+}
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,7 +1,6 @@
 ﻿using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,23 +10,34 @@ namespace WebAPI.Controllers
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repository;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            return Ok(await _repository.GetProductsAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repository.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repository.GetProductTypesAsync());
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repository.GetProductsBrandsAsync());
         }
     }
 }
